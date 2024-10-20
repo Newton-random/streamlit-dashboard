37923588bd2eef08c08ea51a5d0080113e36f42b
Newton-random <kariminewton56@gmail.com>
2024-10-20 20:10:02 +0300

    Add new function to improve feature XYZ

--- app.py
+++ app.py
@@ -5,13 +5,20 @@ import plotly.express as px
 # Load the dataset
 @st.cache
 def load_data():
-    return pd.read_csv('content/dataset.csv')
+    return pd.read_csv('/content/dataset.csv')
 
-data = load_data()
+data = data_path()
 
 # Simple Streamlit app using Plotly
 st.title("Streamlit Dashboard with Plotly")
 st.write("This is a simple example dashboard with Plotly visualizations.")
 
-fig = px.line(data, x="year", y="population", color="sex", title="Population Over Time")
+
+fig = px.line(data, x="year", y="population", color="sex", title="Total inactive population by year and sex")
+fig = px.bar(data, x="year", y="population", color="sex", title="Total inactive population by year and sex")
+fig = px.line(data, x="year", y="population", color="sex", title="Total unemployed population by year and sex")
+fig = px.bar(data, x="year", y="population", color="sex", title="Total unemployed population by year and sex")
+fig = px.line(data, x="year", y="population", color="sex", title="Total employed population by year and sex")
+fig = px.bar(data, x="year", y="population", color="sex", title="Total employed population by year and sex")
+
 st.plotly_chart(fig)
